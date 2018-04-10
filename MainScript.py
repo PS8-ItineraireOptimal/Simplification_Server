e7f6cabbd85e5c17bbda2c27045f932323ff2fc7
衍 <34103504+Jixiong@users.noreply.github.com>
2018-04-10 10:55:27 +0200

    Update MainScript.py
    I have add some comments

--- MainScript.py
+++ MainScript.py
@@ -1,20 +1,34 @@
+####################################################################
+#                                                                  #
+#                     MAIN SCRIPT OF THE DATA BASE                 #      
+#                            version 1.0                           #
+####################################################################
+
+
 import shapefile
 import pandas as pd
+
+#############################################################################################
+# This periode is for read the noeuds and transform it into the type of a two dimension list 
+# with the geographic            
+#############################################################################################
 
 myshp = open("NOEUD_ROUTIER.shp", "rb")
 mydbf = open("NOEUD_ROUTIER.dbf", "rb")
 r = shapefile.Reader(shp=myshp, dbf=mydbf)
-# 在输出的记录中找到经纬度, 假设经纬度分别是第三四个属性
 
 neouds = r.shapeRecords()
 
 liste_neoud = []
 
 for i in range(len(neouds)):
-    liste = neouds[i].record[:] + list(neouds[i].shape.points[0])
+    liste = neouds[i].record[:] + list(neouds[i].shape.points[0]) + 
     liste_neoud.append(liste)
 
-
+#############################################################################################
+# This periode is for read the routes and transform it into a two dimension list with attributs
+# and two points the geographic
+#############################################################################################
 
 myshp = open("TRONCON_ROUTE.shp", "rb")
 mydbf = open("TRONCON_ROUTE.dbf", "rb")
@@ -28,3 +42,7 @@ for name in dir(shape[3]):
         print(name)
 
 print(r.shapeType)
+
+#############################################################################################
+# This periode is for change the geographic data of route into the id of the noeuds
+#############################################################################################

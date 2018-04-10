4d71acf45d4c1df14e77a5f324883149d4cc15be
衍 <34103504+Jixiong@users.noreply.github.com>
2018-04-10 22:28:13 +0200

    add an number of route for noeuds

--- MainScript.py
+++ MainScript.py
@@ -12,7 +12,7 @@ import pandas as pd
 #############################################################################################
 # This periode is for read the noeuds and transform it into the type of a two dimension list 
 # with the geographic    
-# In this list, the terms are noeud_id, type_noeud, nothing, lat, lon, station
+# In this list, the terms are noeud_id, type_noeud, nothing, lat, lon, station, number of the routes
 #############################################################################################
 
 myshp = open("NOEUD_ROUTIER.shp", "rb")
@@ -24,14 +24,15 @@ neouds = r.shapeRecords()
 liste_neoud = []
 
 for i in range(len(neouds)):
-    liste = neouds[i].record[:] + list(neouds[i].shape.points[0]) + [0] #last "0" is for station
+    liste = neouds[i].record[:] + list(neouds[i].shape.points[0]) + [0,0] #last "0" is for station
     liste_neoud.append(liste)
 
 
 #############################################################################################
 # This periode is for read the routes and transform it into a two dimension list with attributs
 # and two points the geographic
-# In this list, the terms are troncon_id, distance, pointA, pointB
+# This periode is for changing the geographic data of route into the id of the noeuds
+# In this list, the terms are troncon_id, distance, id_pointA, id_pointB
 #############################################################################################
 
 myshp = open("TRONCON_ROUTE.shp", "rb")
@@ -42,20 +43,17 @@ troncons = r.shapeRecords()
 
 liste_troncon = []
 
-print(liste_neoud[0][3:-1])
-print(troncons[0].shape.points[0])
-print(liste_neoud[0][3:-1] == list(troncons[0].shape.points[0]))
-
 for i in range(len(troncons)):
     for j in range(len(liste_neoud)):
-        if (liste_neoud[j][3:-1] == list(troncons[i].shape.points[0])): id_pointA = liste_neoud[j][0]
-        if (liste_neoud[j][3:-1] == list(troncons[i].shape.points[-1])): id_pointB = liste_neoud[j][0]
+        if (liste_neoud[j][3:-2] == list(troncons[i].shape.points[0])):
+            id_pointA = liste_neoud[j][0]
+            liste_neoud[j][-1] += 1
+
+        if (liste_neoud[j][3:-2] == list(troncons[i].shape.points[-1])):
+            id_pointB = liste_neoud[j][0]
+            liste_neoud[j][-1] += 1
     liste = [troncons[i].record[0]] + [troncons[i].record[-1]] + [id_pointA] + [id_pointB]
     print(liste)
     liste_troncon.append(liste)
 
 
-
-#############################################################################################
-# This periode is for change the geographic data of route into the id of the noeuds
-#############################################################################################

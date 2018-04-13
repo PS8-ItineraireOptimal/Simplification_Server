d3cb0b3dda2f028a099d1b10794aeec44ed8472b
衍 <34103504+Jixiong@users.noreply.github.com>
2018-04-13 11:40:46 +0200

    clean up the double-path node

--- MainScript.py
+++ MainScript.py
@@ -8,10 +8,7 @@
 
 
 import shapefile
-import copy
-import pandas as pd
 import csv
-import codecs
 
 #############################################################################################
 # This periode is for read the noeuds and transform it into the type of a two dimension list
@@ -28,9 +25,10 @@ neouds = r.shapeRecords()
 liste_neoud = []
 
 for i in range(len(neouds)):
-    liste = [neouds[i].record[0]] + list(neouds[i].shape.points[0]) + [0,0,0,0,0,0,0] #last "0" is for station
+    liste = [neouds[i].record[0]] + list(neouds[i].shape.points[0]) + [0,0,0,0,"", 0,0,0] #last "0" is for station
     liste_neoud.append(liste)
 
+print("read nodes finished.....")
 
 #############################################################################################
 # This periode is for read the routes and transform it into a two dimension list with attributs
@@ -55,24 +53,23 @@ for i in range(10000):
 
         if (liste_neoud[j][1:3] == list(troncons[i].shape.points[0])):
             id_pointA = liste_neoud[j][0]
-            liste_neoud[j][7] += 1
-            if (liste_neoud[j][7] == 1): liste_neoud[j][8] = troncons[i].record[0]
-            if (liste_neoud[j][7] == 2): liste_neoud[j][9] = troncons[i].record[0]
+            liste_neoud[j][8] += 1
+            if (liste_neoud[j][8] == 1): liste_neoud[j][9] = troncons[i].record[0]
+            if (liste_neoud[j][8] == 2): liste_neoud[j][10] = troncons[i].record[0]
             break
 
     for j in range(lonth_noeud):
         if (liste_neoud[j][1:3] == list(troncons[i].shape.points[-1])):
             id_pointB = liste_neoud[j][0]
-            liste_neoud[j][7] += 1
-            if (liste_neoud[j][7] == 1): liste_neoud[j][8] = troncons[i].record[0]
-            if (liste_neoud[j][7] == 2): liste_neoud[j][9] = troncons[i].record[0]
+            liste_neoud[j][8] += 1
+            if (liste_neoud[j][8] == 1): liste_neoud[j][9] = troncons[i].record[0]
+            if (liste_neoud[j][8] == 2): liste_neoud[j][10] = troncons[i].record[0]
             break
 
     liste = [troncons[i].record[0]] + list(troncons[i].record[-2:]) + [id_pointA] + [id_pointB]
     liste_troncon.append(liste)
-    print(liste[0])
 
-
+print("read paths finished 1/4.....")
 
 for i in range(10000):
     zone1  = 10000 + i
@@ -81,25 +78,25 @@ for i in range(10000):
 
         if (liste_neoud[zone2][1:3] == list(troncons[zone1].shape.points[0])):
             id_pointA = liste_neoud[zone2][0]
-            liste_neoud[zone2][7] += 1
-            if (liste_neoud[zone2][7] == 1): liste_neoud[zone2][8] = troncons[zone1].record[0]
-            if (liste_neoud[zone2][7] == 2): liste_neoud[zone2][9] = troncons[zone1].record[0]
+            liste_neoud[zone2][8] += 1
+            if (liste_neoud[zone2][8] == 1): liste_neoud[zone2][9] = troncons[zone1].record[0]
+            if (liste_neoud[zone2][8] == 2): liste_neoud[zone2][10] = troncons[zone1].record[0]
             break
 
     for j in range(lonth_noeud):
         zone2 = (j + 8000) % lonth_noeud
         if (liste_neoud[zone2][1:3] == list(troncons[zone1].shape.points[-1])):
             id_pointB = liste_neoud[zone2][0]
-            liste_neoud[zone2][7] += 1
-            if (liste_neoud[zone2][7] == 1): liste_neoud[zone2][8] = troncons[zone1].record[0]
-            if (liste_neoud[zone2][7] == 2): liste_neoud[zone2][9] = troncons[zone1].record[0]
+            liste_neoud[zone2][8] += 1
+            if (liste_neoud[zone2][8] == 1): liste_neoud[zone2][9] = troncons[zone1].record[0]
+            if (liste_neoud[zone2][8] == 2): liste_neoud[zone2][10] = troncons[zone1].record[0]
             break
 
     liste = [troncons[zone1].record[0]] + list(troncons[zone1].record[-2:]) + [id_pointA] + [id_pointB]
     liste_troncon.append(liste)
-    print(liste[0])
 
 
+print("read paths finished 2/4.....")
 
 for i in range(10000):
     zone1  = 20000 + i
@@ -108,84 +105,106 @@ for i in range(10000):
 
         if (liste_neoud[zone2][1:3] == list(troncons[zone1].shape.points[0])):
             id_pointA = liste_neoud[zone2][0]
-            liste_neoud[zone2][7] += 1
-            if (liste_neoud[zone2][7] == 1): liste_neoud[zone2][8] = troncons[zone1].record[0]
-            if (liste_neoud[zone2][7] == 2): liste_neoud[zone2][9] = troncons[zone1].record[0]
+            liste_neoud[zone2][8] += 1
+            if (liste_neoud[zone2][8] == 1): liste_neoud[zone2][9] = troncons[zone1].record[0]
+            if (liste_neoud[zone2][8] == 2): liste_neoud[zone2][10] = troncons[zone1].record[0]
             break
 
     for j in range(lonth_noeud):
-        zone2 = (j + 16000) % lonth_noeud
+        zone2 = (j + 8000) % lonth_noeud
         if (liste_neoud[zone2][1:3] == list(troncons[zone1].shape.points[-1])):
             id_pointB = liste_neoud[zone2][0]
-            liste_neoud[zone2][7] += 1
-            if (liste_neoud[zone2][7] == 1): liste_neoud[zone2][8] = troncons[zone1].record[0]
-            if (liste_neoud[zone2][7] == 2): liste_neoud[zone2][9] = troncons[zone1].record[0]
+            liste_neoud[zone2][8] += 1
+            if (liste_neoud[zone2][8] == 1): liste_neoud[zone2][9] = troncons[zone1].record[0]
+            if (liste_neoud[zone2][8] == 2): liste_neoud[zone2][10] = troncons[zone1].record[0]
             break
 
     liste = [troncons[zone1].record[0]] + list(troncons[zone1].record[-2:]) + [id_pointA] + [id_pointB]
     liste_troncon.append(liste)
-    print(liste[0])
+
+print("read paths finished 3/4.....")
 
 for i in range(len(troncons) - 30000):
     zone1  = 30000 + i
     for j in range(lonth_noeud):
-        zone2 = (j + 23000) % lonth_noeud
+        zone2 = (j + 24000) % lonth_noeud
+
         if (liste_neoud[zone2][1:3] == list(troncons[zone1].shape.points[0])):
             id_pointA = liste_neoud[zone2][0]
-            liste_neoud[zone2][7] += 1
-            if (liste_neoud[zone2][7] == 1): liste_neoud[zone2][8] = troncons[zone1].record[0]
-            if (liste_neoud[zone2][7] == 2): liste_neoud[zone2][9] = troncons[zone1].record[0]
+            liste_neoud[zone2][8] += 1
+            if (liste_neoud[zone2][8] == 1): liste_neoud[zone2][9] = troncons[zone1].record[0]
+            if (liste_neoud[zone2][8] == 2): liste_neoud[zone2][10] = troncons[zone1].record[0]
             break
 
     for j in range(lonth_noeud):
-        zone2 = (j + 23000) % lonth_noeud
+        zone2 = (j + 8000) % lonth_noeud
         if (liste_neoud[zone2][1:3] == list(troncons[zone1].shape.points[-1])):
             id_pointB = liste_neoud[zone2][0]
-            liste_neoud[zone2][7] += 1
-            if (liste_neoud[zone2][7] == 1): liste_neoud[zone2][8] = troncons[zone1].record[0]
-            if (liste_neoud[zone2][7] == 2): liste_neoud[zone2][9] = troncons[zone1].record[0]
+            liste_neoud[zone2][8] += 1
+            if (liste_neoud[zone2][8] == 1): liste_neoud[zone2][9] = troncons[zone1].record[0]
+            if (liste_neoud[zone2][8] == 2): liste_neoud[zone2][10] = troncons[zone1].record[0]
             break
 
     liste = [troncons[zone1].record[0]] + list(troncons[zone1].record[-2:]) + [id_pointA] + [id_pointB]
     liste_troncon.append(liste)
-    print(liste[0])
 
-
+print("read paths finished.....")
 #############################################################################################
 # This periode will delete all the nodes with only two paths
 # And put those two paths in one
 #############################################################################################
 
 for i in range(len(liste_neoud)):
-    if (liste_neoud[i][7] == 2):
+    if (liste_neoud[i][8] == 2):
 
-        if (liste_troncon[liste_neoud[i][8]-1][3] == liste_neoud[i][0]):
-            if (liste_troncon[liste_neoud[i][9]-1][3] == liste_neoud[i][0]):
-                path1 = liste_troncon[liste_neoud[i][9]-1][4]
-            else:
-                path1 = liste_troncon[liste_neoud[i][9]-1][3]
-            liste_troncon[liste_neoud[i][8]-1][3] = path1
-        else:
-            if (liste_troncon[liste_neoud[i][9]-1][3] == liste_neoud[i][0]):
-                path1 = liste_troncon[liste_neoud[i][9]-1][4]
-            else:
-                path1 = liste_troncon[liste_neoud[i][9]-1][3]
-            liste_troncon[liste_neoud[i][9]-1][4] = path1
+        index_path1 = int(liste_neoud[i][9] - 1)
+        index_path2 = int(liste_neoud[i][10] - 1)
 
-        liste_troncon[liste_neoud[i][8]-1][2] += liste_troncon[liste_neoud[i][9]-1][2]
+        if (index_path1 > 8099): index_path1 = index_path1 - 1
+        if (index_path2 > 8099): index_path2 = index_path2 - 1
+
+        print("i:                            " + str(i))
+        print("index_path1:                  " + str(index_path1))
+        print("index_path1:                  " + str(index_path2))
+        print("liste_troncon[index_path1][3] " + str(liste_troncon[index_path1][3]))
+        print("liste_troncon[index_path2][3] " + str(liste_troncon[index_path2][3]))
+        print("liste_neoud[i][0]             " + str(liste_neoud[i][0]))
+        print("------------------------------------------------------")
+
+        # if (liste_troncon[index_path1][3] == liste_neoud[i][0]):
+        #     if (liste_troncon[index_path2][3] == liste_neoud[i][0]):
+        #         path1 = liste_troncon[index_path2][4]
+        #     else:
+        #         path1 = liste_troncon[index_path2][3]
+        #     liste_troncon[index_path1][3] = path1
+        # else:
+        #     if (liste_troncon[index_path2][3] == liste_neoud[i][0]):
+        #         path1 = liste_troncon[index_path2][4]
+        #     else:
+        #         path1 = liste_troncon[index_path2][3]
+        #     liste_troncon[index_path2][4] = path1
+
+        print("+++++++++++++++++++++++++++++++++++++++++++++++++++++++")
+
+        print(liste_neoud[i][:])
+        liste_troncon[index_path1][2] += liste_troncon[index_path2][2]
 
 
-    liste_neoud[i] = ""
-    liste_troncon[liste_neoud[i][9]-1] = ""
 
+        liste_neoud[i] = ""
+        liste_troncon[index_path2] = ""
 
+while '' in liste_neoud:
+    liste_neoud.remove('')
+while '' in liste_troncon:
+    liste_troncon.remove('')
 
 #############################################################################################
 # This periode is for put the data in a csv
 #
 #############################################################################################
 
-fileHeader1 = ["id_noeud", "lat", "lon", "id_station", "lat_station", "lon_station", "distance_station", "number_route", "path1", "path2"]
+fileHeader1 = ["id_noeud", "lat", "lon", "id_station", "lat_station", "lon_station", "distance_station", "type_station","number_route", "path1", "path2"]
 fileHeader2 = ["id_route", "type_route", "distance", "id_noeud1", "id_noeud2"]
 
 csvFile = open("Liste_noeud.csv", "w", newline ='')
@@ -209,5 +228,3 @@ for i in range(len(liste_troncon)):
     writer.writerow(liste_troncon[i])
 
 csvFile.close()
-
-
